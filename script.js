d4ebf96fa612d52d0eb996f1bdeeabcde12d7733
Jayden221 <143625511+Jayden221@users.noreply.github.com>
2024-02-11 03:01:22 +0000

    literally almost done

--- script.js
+++ script.js
@@ -1,3 +1,181 @@
+console.log("hhh");
+
+let title = document.querySelector(".title");
+  console.log(title);
+
+let options1 = document.querySelector(".options1");
+let options2 = document.querySelector(".options2");
+let options3 = document.querySelector(".options3");
+let options4 = document.querySelector(".options4");
+let options5 = document.querySelector(".options5");
+let options6 = document.querySelector(".options6");
+let options7 = document.querySelector(".options7");
+let options8 = document.querySelector(".options8");
+let options9 = document.querySelector(".options9");
+let options10 = document.querySelector(".options10");
+let options11 = document.querySelector(".options11");
+let options12 = document.querySelector(".options12");
+let options13 = document.querySelector(".options13");
+let options14 = document.querySelector(".options14");
+let options15 = document.querySelector(".options15");
+let options16 = document.querySelector(".options16");
+let options17 = document.querySelector(".options17");
+let options18 = document.querySelector(".options18");
+let options19 = document.querySelector(".options19");
+let options20 = document.querySelector(".options20");
+let options21 = document.querySelector(".options21");
+let options22 = document.querySelector(".options22");
+let options23 = document.querySelector(".options23");
+let options24 = document.querySelector(".options24");
+let options25 = document.querySelector(".options25");
+let options26 = document.querySelector(".options26");
+let options27 = document.querySelector(".options27");
+let options28 = document.querySelector(".options28");
+let options29 = document.querySelector(".options29");
+
+
+let wakeupButton = document.querySelector(".wakeupButton");
+let sleepButton = document.querySelector(".sleepButton");
+let bikeButton = document.querySelector(".bikeButton");
+let walkButton = document.querySelector(".walkButton");
+let continueButton = document.querySelector(".continueButton");
+let confrontButton = document.querySelector(".confrontButton");
+let ignoreButton = document.querySelector(".ignoreButton");
+let nextButton = document.querySelector(".nextButton");
+let forwardButton = document.querySelector(".forwardButton");
+let napButton = document.querySelector(".napButton");
+let classworkButton = document.querySelector(".classworkButton");
+let noButton = document.querySelector(".noButton");
+let onwardButton = document.querySelector(".onwardButton");
+let nopeButton = document.querySelector(".nopeButton");
+let yesButton = document.querySelector(".yesButton");
+let windowButton = document.querySelector(".windowButton");
+let leaveButton = document.querySelector(".leaveButton");
+let persistButton = document.querySelector(".persistButton");
+let carryonButton = document.querySelector(".carryonButton");
+let libraryButton = document.querySelector(".libraryButton");
+let coffeeButton = document.querySelector(".coffeeButton");  
+let easyButton = document.querySelector(".easyButton");
+let takeupButton = document.querySelector(".takeupButton");
+let randomButton = document.querySelector(".randomButton");
+let giveitButton = document.querySelector(".giveitButton");
+let dontgiveButton = document.querySelector(".dontgiveButton");
+let uhohbutton = document.querySelector(".uhohbutton");
+let nothingButton = document.querySelector(".nothingButton");
+let yepButton = document.querySelector(".yepButton");
+let yupButton = document.querySelector(".yupButton");
+let runButton = document.querySelector(".runButton");
+let sureButton = document.querySelector(".sureButton");
+let acceptButton = document.querySelector(".acceptButton");
+let denyButton = document.querySelector(".denyButton");
+let happyButton = document.querySelector(".happyButton");
+let sadButton = document.querySelector(".sadButton");
+let clickthisButton = document.querySelector(".clickthisButton");
+
+
+
+clickthisButton.addEventListener("click", function() {
+  clickthisButton.style.display = "none";
+  options2.style.display = "block";
+  
+});
+
+wakeupButton.addEventListener( "click",function() {
+  wakeupButton.style.display = "none";
+  sleepButton.style.display = "none";
+  options3.style.display = "none";
+  options2.style.display = "none";
+  options3.style.display = "block";
+});
+
+sleepButton.addEventListener( "click",function() {
+  sleepButton.style.display = "none";
+  wakeupButton.style.display = "none";
+  options3.style.display = "none";
+  options2.style.display = "none";
+  options4.style.display ="block";
+});
+
+bikeButton.addEventListener("click",function() {
+  bikeButton.style.display ="none";
+  walkButton.style.display ="none";
+  options3.style.display ="none";
+  options8.style.display ="block";
+});
+
+continueButton.addEventListener("click",function() {
+  continueButton.style.display ="none";
+  options4.style.display ="none";
+  options8.style.display ="block";
+});
+
+walkButton.addEventListener( "click",function() {
+  walkButton.style.display = "none";
+  options3.style.display = "none";
+  options5.style.display = "block";
+});
+
+confrontButton.addEventListener("click",function() {
+  confrontButton.style.display = "none";
+  options5.style.display = "none";
+  options6.style.display = "block";
+});
+
+nextButton.addEventListener("click",function(){
+  nextButton.style.display = "none";
+  options6.style.display = "none";
+  options29.style.display = "block";
+});
+
+ignoreButton.addEventListener("click",function(){
+  ignoreButton.style.display = "none";
+  options5.style.display = "none";
+  options7.style.display = "block";
+});
+
+forwardButton.addEventListener("click",function(){
+  forwardButton.style.display = "none";
+  options7.style.display = "none";
+  options29.style.display= "block";
+});
+                              
+                          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
